26275aa162a47182d5fcb7867d11238da0b243aa
Muhammad Alfaiz Khisma Authar <faizauthar@gmail.com>
2021-06-06 11:52:53 +0700

    ProprietaryChecker: add comment to the output

--- Checker.py
+++ Checker.py
@@ -9,7 +9,10 @@ match_count = 0
 with open('proprietary-files.txt', 'r') as f:
     for line in f:
         line = line.strip('/n')
-        if line in deviceBlobs:
+        if any("#" in s for s in line):
+            print(line)
+
+        elif line in deviceBlobs:
             print(line)
             match_count += 1
             diff_count -= 1
